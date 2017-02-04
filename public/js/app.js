1bcd60720b5286361152fd6e47290a2b22bf8fa4
Maledor <josh.r.cameron@gmail.com>
2017-02-04 17:12:25 -0500

    completed app.js, thereby completing Todo_Too

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+var todoApp = {
+  todos: [],
+  init: function(){
+    todoApp.cacheDom();
+    todoApp.addEventListeners();
+    todoApp.render();
+  },
+  cacheDom: function() {
+    todoApp.createButton = document.querySelector('#create');
+    todoApp.taskInput = document.querySelector('#task');
+    todoApp.categoryInput = document.querySelector('#category');
+    todoApp.dateInput = document.querySelector('#date');
+    todoApp.list = document.querySelector('#list');
+  },
+  render: function(){
+    console.log(todoApp.todos);
+    var listItemsFromTodos = todoApp.todos
+                                    .map(function(todo){
+                                      return `<li>${todo.task}: (${todo.date}) [${todo.category}]</li>`;
+                                    })
+                                    .join('');
+    todoApp.list.innerHTML = listItemsFromTodos;
+  },
+  addEventListeners: function(){
+    todoApp.createButton.addEventListener('click', todoApp.addTodo);
+  },
+  addTodo: function(){
+    var task = todoApp.taskInput.value;
+    var date = todoApp.dateInput.value;
+    var category = todoApp.categoryInput.value;
+    var newTodo = createTodo(task, date, category);
+    todoApp.todos.push(newTodo);
+    todoApp.taskInput.value = '';
+    todoApp.dateInput.value = '';
+    todoApp.categoryInput.value = '';
+    todoApp.render();
+  }
+};
+todoApp.init();
